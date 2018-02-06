ac145d21ed517efce190ecab86cfac5f46bc0434
Philip Luong <pbluong@gmail.com>
2018-02-06 06:41:06 +0000

    Done.

--- loops.js
+++ loops.js
@@ -5,6 +5,7 @@ function forLoop(array){
   return array;
 }
 
+
 function whileLoop(num){
   while(num>0){
     console.log(--num);
@@ -12,9 +13,11 @@ function whileLoop(num){
   return "done";
 }
 
+
 function maybeTrue() {
   return Math.random() >= 0.5;
 }
+
 
 function doWhileLoop(array){
   do {
